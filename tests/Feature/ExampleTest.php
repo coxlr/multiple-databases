fec7973da5a4d0d64f97c958b80d070f1912eade
Lee Cox <lee_cox@sky.com>
2018-07-23 18:13:07 -0700

    wip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,7 +16,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->withoutExceptionHandling();
         $user = factory(\App\User::class)->create();
 
         factory(\App\UserType::class)->create(['user_id' => $user->id]);
